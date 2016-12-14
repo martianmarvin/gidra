2a46ceccb00030c4e9b6e23fc255cf6b78084015
Marvin Bishop <mhb33@protonmail.ch>
2016-12-14 03:42:58 +0000

    More accurate test for sleep task

--- task/sleep/sleep_test.go
+++ task/sleep/sleep_test.go
@@ -3,17 +3,20 @@ package sleep
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/martianmarvin/gidra/task"
 	"github.com/martianmarvin/vars"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSleep(t *testing.T) {
 	tsk := task.New("sleep")
 	ctx := vars.ToContext(context.Background(), vars.New())
 	ctx = vars.SetCtx(ctx, "seconds", 5)
+	before := time.Now()
 	err := tsk.Execute(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	after := time.Now()
+	assert.NoError(t, err)
+	assert.WithinDuration(t, before.Add(5*time.Second), after, time.Second)
 }
