7b126771d5b3b1e060937eb2bdafb8721f7da0f3
Marvin Bishop <mhb33@protonmail.ch>
2017-01-21 06:09:56 +0000

    parser: Simplify proxy parsing

--- script/parser/http.go
+++ script/parser/http.go
@@ -1,12 +1,8 @@
 package parser
 
 import (
-	"fmt"
-	"io"
 	"net/url"
-	"strings"
 
-	"github.com/martianmarvin/gidra/client"
 	"github.com/martianmarvin/gidra/config"
 	"github.com/martianmarvin/gidra/datasource"
 	"github.com/martianmarvin/gidra/script/options"
@@ -39,134 +35,41 @@ func proxyParser(s *options.ScriptOptions, cfg *config.Config) error {
 }
 
 func parseProxy(cfg *config.Config) ([]*url.URL, error) {
-	var proxies []*url.URL
 	// If proxy is a single url, create reader for it
 	rawurl, err := cfg.GetStringE(cfgProxy)
 	if err == nil {
-		u, err := url.Parse(rawurl)
-		if err != nil {
-			return proxies, err
-		}
-		proxies = append(proxies, u)
-		return proxies, nil
+		rawurls := make([]string, 1)
+		rawurls[0] = rawurl
+		return parseURLs(rawurls), nil
 	}
 
 	// Read slice into reader
 	rawurls, err := cfg.GetStringSliceE(cfgProxy)
 	if err == nil {
-		for _, rawurl := range rawurls {
-			u, err := url.Parse(rawurl)
-			if err != nil {
-				return proxies, err
-			}
-			proxies = append(proxies, u)
-		}
-		return proxies, nil
+		return parseURLs(rawurls), nil
 	}
 
 	// Read from input file
 	subcfg := cfg.Get(cfgProxy, nil)
-	reader, err := parseInput(subcfg)
-	if err != nil {
-		return proxies, err
-	}
-	for {
-		row, err := reader.Next()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return proxies, err
-		} else {
-			rawurl := row.GetIndex(0).MustString()
-			u, err := url.Parse(rawurl)
-			if err != nil {
-				return proxies, err
-			}
-			proxies = append(proxies, u)
-		}
-	}
-
-	return proxies, nil
-}
-
-// Reader from single string
-func stringReader(s string) (datasource.ReadableTable, error) {
-	reader, _ := datasource.NewReader("csv")
-	// Add header
-	s = "url\n" + s
-	r := strings.NewReader(s)
-	_, err := reader.ReadFrom(r)
+	fp, err := subcfg.GetStringE(cfgIOSource)
+	rawurls, err = datasource.ReadLines(fp)
 	if err != nil {
 		return nil, err
 	}
-	return reader, nil
+
+	return parseURLs(rawurls), nil
+
 }
 
-// Append scheme if it is not present
-func appendScheme(rawurl, scheme string) string {
-	if strings.Contains(rawurl, "://") {
-		return rawurl
-	}
-	return fmt.Sprintf("%s://%s", strings.ToLower(scheme), rawurl)
-}
-
-// Parse list into a iterable table
-// URLs could be map, list, filename to read from, or url string
-func parseURLList(key string, cfg *config.Config) (*client.URLList, error) {
-	var err error
-	l := client.NewURLList()
-
-	// single URL string
-	u, err := cfg.GetURLE(key)
-	if err == nil {
-		l.Append(u)
-		_, err = l.Next()
-		return l, err
-	}
-
-	// Otherwise look for a list
-	lk := key + "." + cfgIOList
-
-	// array/list of URLs
-	urls, err := cfg.GetStringSliceE(lk)
-	if err == nil {
-		l.AppendString(urls...)
-		_, err = l.Next()
-		return l, err
-	}
-
-	// String representing an input
-
-	// Map with list and optional scheme
-	m, err := cfg.GetStringMapE(key)
-	if err == nil {
-		source, ok := m[cfgIOSource]
-		if !ok {
-			return nil, config.KeyError{Name: cfgIOSource, Err: config.ErrRequired}
-		}
-		lines, err := datasource.ReadLines(source)
+// Parse a slice of string URLs, filling in nil if the url cannot be parsed
+func parseURLs(rawurls []string) []*url.URL {
+	urls := make([]*url.URL, len(rawurls))
+	for i, rawurl := range rawurls {
+		u, err := url.Parse(rawurl)
 		if err != nil {
-			return nil, err
+			continue
 		}
-		urls := make([]*url.URL, 0)
-		for _, line := range lines {
-			u, err := url.Parse(line)
-			if err != nil {
-				return nil, err
-			}
-			urls = append(urls, u)
-		}
-		uType, err := cfg.GetStringE(cfgIOAdapter)
-		if err == nil {
-			uType = strings.ToLower(uType)
-			for _, u := range urls {
-				u.Scheme = uType
-			}
-		}
-		l.Append(urls...)
-		_, err = l.Next()
-		return l, err
+		urls[i] = u
 	}
-
-	return nil, config.ValueError{}
+	return urls
 }
